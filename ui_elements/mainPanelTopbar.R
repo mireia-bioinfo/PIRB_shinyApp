7b17229a497690b942efd9732826250bd8ce084d
Mireia Ramos-Rodríguez <mireiarr9@gmail.com>
2020-09-07 17:10:09 +0200

    Update washu button to be rendered

--- ui_elements/mainPanelTopbar.R
+++ ui_elements/mainPanelTopbar.R
@@ -9,13 +9,8 @@ topbar <-
                         style="color: #fff; background-color: #337ab7; border-color: #2e6da4;",
                         width="100%"))),
     column(1,
-           ## Add washu link
-           actionButton("whashuLink", label="WashU",
-                          icon = icon("link"),
-                          onclick = paste0("window.open(",
-                                           shQuote("http://epigenomegateway.wustl.edu/browser/?genome=hg19&session=62hGf7nfcS&statusId=140947077", type="sh"), ",",
-                                           shQuote("_blank", type="sh"), ")"),
-                          width="100%")),
+           ## Add WashU link to region
+             uiOutput("WashULink")),
     column(1,
            ## Add UCSC link to region
              uiOutput("UCSCLink")),
