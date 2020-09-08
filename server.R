8f2708e49c59176ebb11b3cfec39c40a97c72c4e
Mireia Ramos <mireiarr9@gmail.com>
2020-09-08 10:04:04 +0200

    Fix WashU link

--- server.R
+++ server.R
@@ -199,7 +199,7 @@ server <- function(input, output, session) {
   })
 
     output$WashULink <- renderUI({
-    url <- paste0("http://epigenomegateway.wustl.edu/legacy/?genome=hg19&datahub_jsonfile=https://raw.githubusercontent.com/mireia-bioinfo/IRB_hub/master/washu_session_pchic.txt&position=",
+    url <- paste0("http://epigenomegateway.wustl.edu/legacy/?genome=hg19&datahub_jsonfile=https://raw.githubusercontent.com/mireia-bioinfo/IRB_hub/master/washu_session_pchic.txt&coordinate=",
                   input$chr, ":", input$start, "-", input$end)
 
     actionButton("WashULink", label="WashU",
